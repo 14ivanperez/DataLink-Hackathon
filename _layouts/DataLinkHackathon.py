0097b263b3609a70c1fd7ff9ec02f0a801e1a154
Ivan <ivvanperez14@gmail.com>
2023-04-28 20:21:12 +0200

    predict data with logistic regression

--- _layouts/DataLinkHackathon.py
+++ _layouts/DataLinkHackathon.py
@@ -2,10 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
-from sklearn.metrics import confusion_matrix, classification_report, roc_curve, auc
+from sklearn.preprocessing import OneHotEncoder
 
 # Read CSV file
 df = pd.read_csv('C:/Users/ivvan/Desktop/Python/credit_traindata.csv')
@@ -41,44 +38,35 @@ desc_stats.plot(kind='bar').get_figure().savefig("estadisticas_descriptivas.jpg"
 
 
 # Divide data in training and test
-# Define variables X and Y
-X = df.iloc[:, :-1]
-y = df['class']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+# Define variables X and Y and separate variable to predict
+y = df["class"]
 
+# select all variables except variable to predict
+X = df.drop("class", axis=1)
 
-# Entrenamiento del modelo: Regresión logística
-lr = LogisticRegression()
-lr.fit(X_train, y_train)
-y_pred_lr = lr.predict(X_test)
+# select categorical variables and apply one-hot codification
+from sklearn.preprocessing import OneHotEncoder
+categorical_cols = X.select_dtypes(include=["object"]).columns
+encoder = OneHotEncoder()
+X_cat = encoder.fit_transform(X[categorical_cols])
 
-modelo = LogisticRegression()
+# select numerical variables
+numerical_cols = X.select_dtypes(include=["int64", "float64"]).columns
+X_num = X[numerical_cols]
 
-# Entrenar el modelo con los datos de entrenamiento
-modelo.fit(X_train, y_train)
+# put together both types of variables
+import scipy.sparse as sp
+X_encoded = sp.hstack((X_cat, X_num))
 
-# Predecir los valores de la variable de salida para los datos de prueba
-y_pred = modelo.predict(X_test)
+# split data intro training and testing
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X_encoded, y, test_size=0.2, random_state=42)
 
-
-
-# Definir variables X e y
-X = df.iloc[:, :-1] # Todas las variables excepto la última
-y = df.iloc[:, -1] # Variable objetivo "Loan_Status"
-
-# Dividir los datos en conjuntos de entrenamiento y prueba
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-# Instanciar modelo de regresión logística
+# adjust model to logistic regression
+from sklearn.linear_model import LogisticRegression
 model = LogisticRegression()
-
-# Ajustar modelo con datos de entrenamiento
 model.fit(X_train, y_train)
 
-# Realizar predicciones con datos de prueba
+# make predictions with testing data
 y_pred = model.predict(X_test)
 
-# Evaluar rendimiento del modelo
-print("Accuracy:", accuracy_score(y_test, y_pred))
-print("Confusion Matrix:\n", confusion_matrix(y_test, y_pred))
-print("Classification Report:\n", classification_report(y_test, y_pred))
