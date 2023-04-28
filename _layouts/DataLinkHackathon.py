2d6824ad183f67d53480f3c5913c3a7cdf601b17
Ivan <ivvanperez14@gmail.com>
2023-04-28 19:39:25 +0200

    Start data preparation splitting data

--- _layouts/DataLinkHackathon.py
+++ _layouts/DataLinkHackathon.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.model_selection import train_test_split
 
 # Read CSV file
 df = pd.read_csv('C:/Users/ivvan/Desktop/Python/credit_traindata.csv')
@@ -30,3 +31,10 @@ desc_stats.to_csv('desc_stats.csv')
 
 #Save descriptive statistics as an image
 desc_stats.plot(kind='bar').get_figure().savefig("estadisticas_descriptivas.jpg")
+
+
+# Divide data in training and test
+# Define variables X and Y
+X = df.iloc[:, :-1]
+y = df['class']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
