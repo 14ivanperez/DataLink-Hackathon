7fa63b4117ea9d2c5a4e4b9ecd1614ee3aa8b713
Ivan <ivvanperez14@gmail.com>
2023-04-28 19:17:59 +0200

    Start Exploratory Data Analysis

--- _layouts/DataLinkHackathon.py
+++ _layouts/DataLinkHackathon.py
@@ -1,5 +1,30 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 # Read CSV file
 df = pd.read_csv('C:/Users/ivvan/Desktop/Python/credit_traindata.csv')
+
+# Exploratory Data Analysis
+
+# Select numerical variables
+numeric_vars = ['duration', 'credit_amount', 'employment', 'personal_status', 'num_dependents']
+
+# Distribution graphs
+for col in numeric_vars:
+    sns.displot(df[col], kde=True)
+    plt.title(f'Distribution of {col}')
+    plt.savefig(f'{col}_distplot.jpg')
+    plt.show()
+
+# Correlation graph
+corr = df[numeric_vars].corr()
+sns.heatmap(corr, annot=True, cmap='coolwarm')
+plt.title('Correlation Matrix')
+plt.savefig('correlation_matrix.jpg')
+plt.show()
+
+# Descriptive statistics
+desc_stats = df[numeric_vars].describe()
+desc_stats.to_csv('desc_stats.csv')
+
